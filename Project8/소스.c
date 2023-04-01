7ac1cca56f996a0f823d6c4d4151d42dfdd754f4
kauzue <88610807+kauzue@users.noreply.github.com>
2023-04-01 22:41:33 +0900

    Add files via upload

---
+++ Project8/소스.c
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+#include "setting.h"
+
+int main(void)
+{
+	int* repeat = 1;
+	InitSnake();
+	
+	while (repeat) {
+		system("cls");
+
+		UpdateSnake(&repeat);
+		DrawSnake();
+
+		Sleep(250);
+	}
+}
